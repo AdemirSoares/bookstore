63d4aa6c227d7a3255c7662c170e9af50ae19edb
ademirsoares <ademir@licitaronline.com>
2025-06-06 12:17:34 -0300

    Criando testes

--- order/serializers/order_serializers.py
+++ order/serializers/order_serializers.py
@@ -1,17 +1,25 @@
 from rest_framework import serializers
 
+from order.models import Order
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class OrderSerializer(serializers.ModelSerializer):
-    product = ProductSerializer(required=True, many=True)
+    product = ProductSerializer(many=True, read_only=True)
+    product_ids = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Product.objects.all(), write_only=True, source='product'
+    )
     total = serializers.SerializerMethodField()
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
 
     def get_total(self, instance):
-        total = sum([product.price for product in instance.product.all()])
-        return total
+        return sum([product.price for product in instance.product.all()])
 
     class Meta:
-        model = Product
-        fields = ['product', 'total']
+        model = Order
+        fields = ['user', 'product', 'product_ids', 'total']
